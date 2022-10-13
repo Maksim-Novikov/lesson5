4f9d283c52dfdbbbd66874453e6ba65eccb6cf40
maksim_novikov <rodgersdevelop@gmail.com>
2022-10-13 20:24:12 +0600

    initial commit

---
+++ app/src/main/java/com/maksimnovikov/lesson5/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.maksimnovikov.lesson5
+
+import androidx.appcompat.app.AppCompatActivity
+
+const val OTHER_NAME: Int = 1
+
+class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//        OTHER_NAME
+//    }
+}
